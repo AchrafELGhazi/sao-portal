69a4841afeb8d44ceb641a4d29357fab2fd66778
AchrafElGhazi <a.elghazi@aui.ma>
2025-03-27 04:46:01 +0000

    hfg

--- src/types/clubs.ts
+++ src/types/clubs.ts
@@ -1,27 +1,24 @@
 
-// For board member positions and their names
+
 export interface BoardMember {
   [position: string]: string | undefined;
 }
 
-// For semesters with their board members
+
 export interface BoardMembers {
   [semester: string]: BoardMember | undefined;
 }
 
-// For events in a semester
 export interface SemesterEvents {
   [semester: string]: string[] | undefined;
 }
 
-// The main Club interface
 export interface Club {
   clubName: string;
   boardMembers?: BoardMembers;
   events?: SemesterEvents;
 }
 
-// Club type categories
 export interface ClubType {
   id: string;
   name: string;
