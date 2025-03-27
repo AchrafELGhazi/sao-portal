774dbd51419579b0269c25d31fc059144f49d4be
AchrafElGhazi <a.elghazi@aui.ma>
2025-03-27 02:47:01 +0000

    lk

---
+++ src/data/clubs/cluturalClubsData.ts
@@ -0,0 +1,542 @@
+const culturalClubsData = [
+  {
+    "clubName": "Islamic Art and Culture",
+    "boardMembers": {
+      "Fall 2021": {
+        "President": "Med Amine El Mesaoudi",
+        "Vice-President": "Kawtar Kettani",
+        "GS": "Aicha Mayif",
+        "Treasurer": "Loubna El Hafidi",
+        "Advisor": "Dr. Naeem Nisar Sheikh"
+      },
+      "Spring 2022": {
+        "President": "Kawtar Kettani",
+        "Vice-President": "Imane Fouad",
+        "GS": "vacant",
+        "Treasurer": "Med Amine El Mesaoudi",
+        "Advisor": "Dr. Naeem Nisar Sheikh"
+      },
+      "Fall 2024": {
+        "President": "Yassine Darouichi",
+        "Vice-President": "Amine Lahnin",
+        "GS": "Oumaima Rtal",
+        "Treasurer": "Younes Kettani",
+        "Advisor": "Naeem Nisar Sheikh"
+      }
+    },
+    "events": {
+      "Fall 2021": [
+        "The Science Of Hadith (Lecture + Open Discussion)",
+        "Al-Israe Wal-Mi'Raj: The Story Of the night Miraculous Night (Story Telling)",
+        "Religion VS Culture",
+        "The Four Madhabih of Islam (Talk presented by Dr.Naeem Nisar Sheikh)",
+        "Tajweed Tutoring",
+        "Workshop: LGBTQ+ And Islam",
+        "Talk about: Homosexuality and Islam (Presented by Dr.Noeem Nisar Sheikh)",
+        "Amdah and Samaa",
+        "Quiz Time 'Youths and Islam'",
+        "Science and Islam (Guest speaker: Dr. Abdullah Travathan)",
+        "Cohabitation in our society",
+        "Self Discipline Prayer (Salat)",
+        "Religion Identity in a Globalized World (Guest speaker: Yassine El Amri)"
+      ],
+      "Spring 2022": [
+        "Valentine's Celebration In Islam",
+        "Journey from Judaism to Islam",
+        "Special Places and Times in Islam (Friendly Contest)",
+        "Story Time: Converting to Islam (Guest speaker: Prf.Taylor Robb",
+        "Our Origins:A discussion about Evolution",
+        "Ramadan Opportunity To Change (A short Talk and friendly discussion)",
+        "Hijab: An Empowerment or a Restriction? (Guest Speaker; Pr.Monica Festa)",
+        "The effects of faith on human behavior and morals (presented by: Mr.Abdesslam Tahiri)",
+        "Why do I have to fast in Ramadan",
+        "Why should Allah punishes us if he already knows that we will sin?"
+      ],
+      "Fall 2022": [
+        "How to Preserve Our Islamic Identity",
+        "My Journey to Islam (by Dr.Stephen Trevathan)",
+        "Contest Seerah Nabawiyyah",
+        "Movie night:(Great Muslim Figure from the 20th Century)",
+        "Workshop: Pornography Addiction by Dr.Naeem Sheikh",
+        "Propaganda Nowadays Introduction and Discussion",
+        "Dealing with Academic Stress (Speaker: Pr.Hicham Marbouh)",
+        "The Crisis in The Modern Economics and The Islamic Alternatives (Talk and Critical Analysis by: Dr.Muneeb Hussain Gatto",
+        "Friendly Islamic Contest"
+      ],
+      "Spring 2023": [
+        "Freedom of Speech",
+        "How Islam Came to Morocco (Guest speaker: Professor El Hassan Al Azhari)",
+        "First humanitarian event:Welcome 44 children from Center 'Nawat Tamayouz' in region El Mers",
+        "Movie Night: Islam and Science",
+        "How Global Media Treats Muslim Speakers",
+        "Ramadan Contest",
+        "Salat 'Final Wasiyab of the Prophet PBUH'",
+        "Cross-Cultural Exchange (AUI Students share their stories)",
+        "Thinking about relationships as college student",
+        "Steadfastness (Istiqamah) in our University Life"
+      ],
+      "Fall 2023": [
+        "Islam and Astronomy",
+        "How the Prophet PBUH dealt with the youth",
+        "Contest about Seerah Al Nabawiyah",
+        "Etiquette of Responding to Mistreatment in Islam",
+        "Movie Screening 'Hijrah'",
+        "Palestinian Issue Between History,Media,Reality (Panel Discussion by: Abdullah Trevathan, Dr.Taylor Robb, Dr.Naeem Sheikh)",
+        "Trip to Maarid Seerah in Rabat",
+        "Quran Recitation Competition for kids (Collaboration with UNICEF Club)"
+      ],
+      "Spring 2024": [
+        "Islam and Identity: Where do we stand",
+        "Gender:Social VS Biological",
+        "Cultural Exchange: Italians in the Muslim World",
+        "A contest on: Different themes of General Islamic knowledge",
+        "How to prepare ourselves for Ramadan",
+        "Development of the Prophets Mosque:From Hijra to now",
+        "Cross-cultural exchange: AUI student share their stories",
+        "The Madahib of Islam",
+        "The last 10 days of Ramadan: Importance of Laylatul'Qadr",
+        "Mirage oh Atheism: A Quranic Response",
+        "Does Science Udermine Islam? (A talk by Pr. Hamza Tzortzis)"
+      ],
+      "Summer 2024": [
+        "Islam and evolution"
+      ],
+      "Fall 2024": [
+        "Quizz Night",
+        "From where should we get out knowledge",
+        "Islam In Russia and Central Asia",
+        "Pornography Addiction: Talk and Workshop",
+        "Talk: Islam in Russia and Central Asia",
+        "The Mirage of Atheism: A Quranic Response"
+      ]
+    }
+  },
+  {
+    "clubName": "Palestine Culture Club",
+    "boardMembers": {
+      "Fall 2024": {
+        "President": "El Mehdi Laidoudi",
+        "Vice-President": "Ouadia Allabou",
+        "GS": "Israe Dedak",
+        "Treasurer": "Rizk Chafik El Ishaki",
+        "Advisor": "Dr.Driss Maghraoui"
+      }
+    },
+    "events": {
+      "Fall 2024": [
+        "Palestine: History,Faith,and Land (Discussion Panel) Part 1",
+        "Palestine: History,Faith,and Land (Discussion Panel) Part 2",
+        "The other side of the story of Palestine: When Israeli Revisionist Historians Speak Out. (Talk by Dr.Driss Maghraoui)",
+        "Israelism:a documentary",
+        "Why is Palestine important?",
+        "Lavender,Gospel, and where's dad"
+      ]
+    }
+  },
+  {
+    "clubName": "Turkish Club",
+    "boardMembers": {
+      "Fall 2023": {
+        "President": "Aicha Almaddah",
+        "Vice-President": "Noussayba Otmani",
+        "GS": "Aya Drissi",
+        "Treasurer": "Khaoula Barbache",
+        "Advisor": "Dr.Onur Ozsoy"
+      },
+      "Spring 2024": {
+        "President": "Fatima Zahra Lantrin",
+        "Vice-President": "Noussayba Otmani",
+        "GS": "Aya Drissi",
+        "Treasurer": "Khaoula Barbache",
+        "Advisor": "Dr.Onur Ozsoy"
+      },
+      "Summer 2024": {
+        "President": "Fatima Zahra Lantrin",
+        "Vice-President": "Noussayba Otmani",
+        "GS": "Aya Drissi",
+        "Treasurer": "Khaoula Barbache",
+        "Advisor": "Dr.Onur Ozsoy"
+      },
+      "Fall 2024": {
+        "President": "Fatima Zahra Lantrin",
+        "Vice-President": "Noussayba Otmani",
+        "GS": "Aya Drissi",
+        "Treasurer": "Khaoula Barbache",
+        "Advisor": "Dr.Onur Ozsoy"
+      }
+    },
+    "events": {
+      "Fall 2023": [
+        "Turkish Classes",
+        "Visit turkish embassy",
+        "Movie night",
+        "Movie night (collaboration with Cinematic Corner)",
+        "Cultural week: stand (Baklawa, Houlkom, turkish tea, turkish coffee, turkish clothes)"
+      ],
+      "Spring 2024": [
+        "Turkish classes",
+        "Movie nights",
+        "Karaoke nights",
+        "Cultural week with OIP"
+      ],
+      "Fall 2024": [
+        "Turkish Classes",
+        "Karaoke nights",
+        "Visit turkish embassy"
+      ]
+    }
+  },
+  {
+    "clubName": "Nexus",
+    "boardMembers": {
+      "Spring 2024": {
+        "President": "Rania Daya",
+        "Vice-President": "Islam"
+      },
+      "Fall 2024": {
+        "President": "Islam"
+      }
+    },
+    "events": {
+      "Spring 2024": [
+        "Trip to Rabat ( Musée des poupées...)",
+        "Nexus x MPC: Morocco and Algeria",
+        "Double Dare x CLE"
+      ],
+      "Fall 2024": [
+        "North Trip",
+        "Trivia night",
+        "Guest Talk: Elham Golpushnezhad: Hip Hop's roots in African American Muslim Culture",
+        "Holi Event",
+        "Trip to Rabat",
+        "Catch me if you can x CLE",
+        "Volubilis Trip",
+        "Cultural Fair x OIP",
+        "Azzemmour Trip",
+        "Girls Party"
+      ]
+    }
+  },
+  {
+    "clubName": "aytmaten",
+    "boardMembers": {
+      "Spring 2022": {
+        "President": "hassan chabakh",
+        "Vice-President": "hanae wazzifi"
+      }
+    },
+    "events": {
+      "Spring 2022": [
+        "tamazight classes",
+        "traditional carpets fundraising"
+      ]
+    }
+  },
+  {
+    "clubName": "Japanese",
+    "boardMembers": {
+      "Fall 2021": {
+        "President": "rim sedki",
+        "Vice-President": "aya bakir",
+        "Advisor": "m'hamed chraibi"
+      },
+      "Spring 2022": {
+        "President": "nada ziane",
+        "Vice-President": "mostafa idrissi",
+        "Advisor": "m'hamed chraibi"
+      },
+      "Fall 2022": {
+        "President": "oualid hajji sabri",
+        "Vice-President": "adam koubi",
+        "General Secretary": "Nour El Moujahid",
+        "Advisor": "m'hamed chraibi"
+      },
+      "Spring 2023": {
+        "President": "oualid hajji sabri",
+        "Vice-President": "adam koubi",
+        "Advisor": "m'hamed chraibi"
+      },
+      "Fall 2023": {
+        "President": "oualid hajji sabri",
+        "Vice-President": "adam koubi",
+        "Advisor": "m'hamed chraibi"
+      },
+      "Spring 2024": {
+        "President": "Mohammed Benabdallah Belbaraka",
+        "Vice-President": "Ghita Mandri",
+        "Advisor": "m'hamed chraibi"
+      },
+      "Fall 2024": {
+        "President": "Mohammed Benabdallah Belbaraka",
+        "Vice-President": "Ghita Mandri",
+        "Advisor": "m'hamed chraibi"
+      }
+    },
+    "events": {
+      "Fall 2021": [
+        "cooking workshop",
+        "language classes",
+        "anime screenings"
+      ],
+      "Spring 2022": [
+        "Japanese classes",
+        "valentine's day",
+        "maid's cafe",
+        "anime screenings"
+      ],
+      "Summer 2022": [
+        "japanese cultural festival",
+        "anime screenings"
+      ],
+      "Fall 2022": [
+        "maid's cafe",
+        "anime screenings"
+      ],
+      "Spring 2023": [
+        "Yazaki cultural exhibition",
+        "Japanese Culture through Workshops",
+        "haru matsuri",
+        "anime screenings"
+      ],
+      "Fall 2023": [
+        "murder mystery",
+        "festive Japanese cafe",
+        "anime screenings"
+      ],
+      "Spring 2024": [
+        "Trivia Night",
+        "rahma collaboration",
+        "game tournaments",
+        "anime screenings"
+      ],
+      "Fall 2024": [
+        "movies nights",
+        "collab cinematic corner"
+      ]
+    }
+  },
+  {
+    "clubName": "sufism",
+    "boardMembers": {
+      "Fall 2022": {
+        "President": "bakr ouzid",
+        "Vice-President": "salma rachidi",
+        "Advisor": "abderafie el maktoub"
+      },
+      "Spring 2023": {
+        "President": "bakr ouzid",
+        "Vice-President": "salma rachidi",
+        "Advisor": "abderafie el maktoub"
+      },
+      "Fall 2023": {
+        "President": "mohamed ismaili",
+        "Vice-President": "zahra biadi",
+        "Advisor": "abderafie el maktoub"
+      },
+      "Spring 2024": {
+        "President": "mohamed ismaili",
+        "Vice-President": "zahra biadi",
+        "Advisor": "abderafie el maktoub"
+      }
+    },
+    "events": {
+      "Fall 2022": [
+        "trip to zaouia al qadira al boutchichia",
+        "fes festival of sufi culture",
+        "roundtable discussion"
+      ],
+      "Spring 2023": [
+        "samaa sufi masterclasses"
+      ],
+      "Fall 2023": [
+        "samaa sufi masterclasses"
+      ]
+    }
+  },
+  {
+    "clubName": "interfaith alliance",
+    "boardMembers": {
+      "Fall 2021": {
+        "President": "soundous charmrah",
+        "Vice-President": "salma el moustafid",
+        "Advisor": "karen smith"
+      },
+      "Spring 2022": {
+        "President": "soundous charmrah",
+        "Vice-President": "ayoub el mabkhout",
+        "Advisor": "karen smith"
+      },
+      "Fall 2022": {
+        "President": "ayoub el mabkhout",
+        "Vice-President": "najma ibrahimi",
+        "Advisor": "karen smith"
+      },
+      "Spring 2023": {
+        "President": "ayoub el mabkhout",
+        "Vice-President": "najma ibrahimi",
+        "Advisor": "karen smith"
+      },
+      "Fall 2023": {
+        "President": "roumaisae medouar",
+        "Vice-President": "wessal kheir bouaddi",
+        "Advisor": "karen smith"
+      },
+      "Spring 2024": {
+        "President": "roumaisae medouar",
+        "Vice-President": "wessal kheir bouaddi",
+        "Advisor": "karen smith"
+      },
+      "Fall 2024": {
+        "President": "wessal kheir bouaddi",
+        "Vice-President": "joumana ammara",
+        "Advisor": "karen smith"
+      }
+    },
+    "events": {
+      "Fall 2021": [
+        "day of the dead",
+        "diwali",
+        "visit to the monestry in Midelt",
+        "jewish newyear",
+        "movie screening of gods and men",
+        "intefaith hike: sidi abdessalam",
+        "Thanksgiving"
+      ],
+      "Spring 2022": [
+        "mexican egghunt",
+        "visit to the monestry in Midelt",
+        "jewish passover celebration",
+        "movie screening of gods and men",
+        "interfaith ftour",
+        "christian passover celebration",
+        "intefaith hike: sidi abdessalam",
+        "hanukah celebration",
+        "xmas celebration"
+      ],
+      "Fall 2022": [
+        "day of the dead",
+        "diwali",
+        "visit to the monestry in Midelt",
+        "jewish newyear",
+        "movie screening of gods and men",
+        "intefaith hike: sidi abdessalam",
+        "Thanksgiving"
+      ],
+      "Spring 2023": [
+        "mexican egghunt",
+        "visit to the monestry in Midelt",
+        "jewish passover celebration",
+        "movie screening of gods and men",
+        "interfaith ftour",
+        "christian passover celebration",
+        "intefaith hike: sidi abdessalam",
+        "hanukah celebration",
+        "xmas celebration"
+      ],
+      "Fall 2023": [
+        "day of the dead",
+        "diwali",
+        "visit to the monestry in Midelt",
+        "jewish newyear",
+        "movie screening of gods and men",
+        "intefaith hike: sidi abdessalam",
+        "Thanksgiving"
+      ],
+      "Spring 2024": [
+        "mexican egghunt",
+        "visit to the monestry in Midelt",
+        "jewish passover night",
+        "movie screening gods and men",
+        "interfaith ftour",
+        "christian passover",
+        "intefaith hike",
+        "hanukah celebration",
+        "xmas celebration"
+      ],
+      "Fall 2024": [
+        "weekly discussion",
+        "Workshop on Gratitude",
+        "Movie Night of gods and man",
+        "Talk by the Mexican counsal general about Mexican Arab heritage followed by a discussion",
+        "day of the dead",
+        "diwali",
+        "visit to the monestry in Midelt",
+        "jewish newyear",
+        "movie screening gods and men",
+        "intefaith hike",
+        "Thanksgiving",
+        "weekly discussion",
+        "visit to monestary in toumlili"
+      ]
+    }
+  },
+  {
+    "clubName": "Chinese",
+    "boardMembers": {
+      "Fall 2022": {
+        "President": "rim touareg",
+        "Vice-President": "aya khadda",
+        "General Secretary": "Nour El Moujahid"
+      }
+    },
+    "events": {
+      "Spring 2023": [
+        "Movie Night",
+        "Silk Scrunchies workshop",
+        "Collab with Honors Program",
+        "Stickers sales",
+        "Tattoo stand"
+      ],
+      "Fall 2023": [
+        "General Meeting",
+        "Movie Night",
+        "Lanterns workshop"
+      ]
+    }
+  },
+  {
+    "clubName": "Riff ino",
+    "boardMembers": {
+      "Spring 2024": {
+        "President": "Wiam Boucetta",
+        "Vice-President": "Narjiss Tayri",
+        "General Secretary": "Douae Lakhlifi",
+        "Treasurer": "kenza bounaerra's friend"
+      }
+    },
+    "events": {
+      "Spring 2024": [
+        "General Meeting",
+        "Ladies Night",
+        "Tarifit Classes",
+        "Movie Screening"
+      ]
+    }
+  },
+  {
+    "clubName": "ALC",
+    "boardMembers": {
+      "Fall 2021": {
+        "President": "nissrine Sadik",
+        "Vice-President": "Nouhaila Babour",
+        "General Secretary": "Khaoula El Fellahi",
+        "Treasurer": "Manal Mkamel",
+        "Advisors": "mMeriem Sahli"
+      },
+      "Fall 2022": {
+        "President": "Khaoula El Fellahi"
+      }
+    },
+    "events": {
+      "Fall 2021": [
+        "Darija/Arabic classes"
+      ],
+      "Fall 2022": [
+        "Creative Writing Competition",
+        "Arabic Poetry Day",
+        "Trip to Volubilis",
+        "Palestinian Day"
+      ]
+    }
+  }
+];
