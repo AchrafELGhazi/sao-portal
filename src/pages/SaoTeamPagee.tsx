54b8c975899a9faea0bb001eccbe686c03bb5ecf
AchrafElGhazi <a.elghazi@aui.ma>
2025-03-27 04:42:33 +0000

    gd

--- src/pages/SaoTeamPagee.tsx
+++ src/pages/SaoTeamPagee.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useRef } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import {
-  ArrowLeft,
   ChevronRight,
   ChevronDown,
   ChevronUp,
